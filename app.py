504e764c6974aa4e8437f2be4b2a36e00fecee95
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-08-04 12:46:21 +0300

    display genres as list

--- app.py
+++ app.py
@@ -183,7 +183,7 @@ def show_venue(venue_id):
   data={
     "id": venue.id,
     "name": venue.name,
-    "genres": venue.genres,
+    "genres": venue.genres[1:-1].split(','),
     "address": venue.address,
     "city": venue.city,
     "state":venue.state,
